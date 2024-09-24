86e8f0f9e2778266fd250f9b214cd4725700fc49
Sathvik Andhavarapu <sandhav1@terpmail.umd.edu>
2024-09-24 16:41:09 -0400

    .

--- recycle.sh
+++ recycle.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# takes 1. container name 2. 
+# takes 1. container name 2. container ip
+if [ $# -ne 2 ]
+then
+    echo "Please enter 1. container name and 2. container ip"
+    exit 1
+fi
+
+ctr_name=$1
+ctr_ip=$2
 
 # set up / redeploy the container with NAT rules
+    # maybe we have some snapshot or something to have the saved configuration
 
 # shut down the container when attacker leaves
     # find mitm logs
     # grep out log out
 
 # randomize banner message
-    # maybe pick from premade configurations?
 
-num=$(( 1 + $RANDOM % 4 ))
+rand=$(( 1 + $RANDOM % 4 )) # use this number to edit the banner message (from banners.txt?)
 
 # redeploy (NAT rules)
 
--- startup.sh
+++ startup.sh
@@ -5,4 +5,4 @@
 node mitm.js -n <container name> -i <container internal IP> -p <MITM listening port> --auto-access --auto-access-fixed <number of attempts before allowing access> --debug
 
 # run MITM in the background
-forever -l <full path of log file> start mitm.js <mitm options>
+sudo forever -l ~/$1_log start ~/MITM/mitm.js -n $1 -i
