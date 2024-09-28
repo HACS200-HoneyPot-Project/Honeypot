29b3179a2698b08c5664d2e40a83bb900c6dc829
devp2303 <devp2303@gmail.com>
2024-09-28 01:10:27 -0400

    deploy a container with NAT routing and banner

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,24 @@
 
 # nat rules prerouting to strart it up again
 
+if [[ $# != 2 ]]; then
+	echo "Usage: $0 container_name external_ip"
+	exit 1
+fi
 
+container_name=$1
+external_ip=$2
+container_ip=$(sudo lxc-info -n "$container_name" -iH)
+banner_file="banners.txt"
 
+sudo lxc-create -n "$1" -t download -- -d ubuntu -r focal -a amd64
+sudo lxc-start -n "$1"
 
+sudo ip addr add "$1"/16 brd + dev eth0
+sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 -- destination "$2" --jump DNAT --to-destination "$container_ip"
+sudo iptables --table nat --insert POSTROUTING --source "$container_ip" --destination 0.0.0.0/0 --jump SNAT --to-source "$2"
 
+banner_message=$(shuf -n 1 "$banner_file")
+# This is how chat gpt said to display the banner in the container 
+# Insert the banner into the container's /etc/motd (Message of the Day)
+echo "$banner_message" | sudo tee /var/lib/lxc/"$container_name"/rootfs/etc/motd > /dev/null
