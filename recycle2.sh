9abcbbc0334685d21f6a85a97b33df7bfa8d6cc7
Sathvik Andhavarapu <sandhav1@terpmail.umd.edu>
2024-09-29 14:06:45 -0400

    recycling script

--- recycle2.sh
+++ recycle2.sh
@@ -13,30 +13,6 @@
 # nat rules prerouting to strart it up again
 
 
-###### MY NOTES 
-pot_name = $1
-pot_ip = $2
-banner_message=$(shuf -n 1 "banners.txt") # to randomly get a banner message
-
-my_ip="172.30.250.127" - idk 
-
-sudo lxc-create -n "$pot_name" -t download -- -d ubuntu -r focal -a amd64
-sudo lxc-start -n "$pot_name"
-
-sudo ip addr add "$my_ip"/16 brd + dev eth0
-sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 -- destination "$my_ip" --jump DNAT --to-destination "$container_ip"
-sudo iptables --table nat --insert POSTROUTING --source "$container_ip" --destination 0.0.0.0/0 --jump SNAT --to-source "$my_ip"
-
-
-sudo ip addr delete "$my_ip"/16 brd + dev eth0
-sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 -- destination "$my_ip" --jump DNAT --to-destination "$container_ip"
-sudo iptables --table nat --delete POSTROUTING --source "$container_ip" --destination 0.0.0.0/0 --jump SNAT --to-source "$my_ip"
-
-
-sudo lxc-stop -n "$some_name"
-sudo lxc-destroy -n "$some_name"
-
-
 # TO DESTROY THE CONTAINER
 # PARAMETERS ARE THE CONTAINERS NAME AND THE IP ASSOCIATED
 # CONTAINER IP CAN GOTTEN FROM THIS SCRIPT I THINK
