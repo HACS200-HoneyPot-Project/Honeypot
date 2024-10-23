49800736a36f0cccf7623319b98884f55947cc23
marissabc <mcarper@terpmail.umd.edu>
2024-10-23 16:34:48 -0400

    deployed code

---
+++ testing_start.sh
@@ -0,0 +1,271 @@
+deploying container1 128.8.238.108
+in for loop 1
+in for loop 2
+in for loop 3
+in for loop 4
+in for loop 5
+in for loop 1
+in for loop 2
+in for loop 3
+in for loop 4
+in for loop 5
+in for loop 1
+in for loop 2
+in for loop 3
+in for loop 4
+in for loop 5
+in for loop 1
+deploying container1 128.8.238.183
+in for loop 2
+deploying container2 128.8.238.32
+in for loop 3
+deploying container3 128.8.238.51
+in for loop 4
+deploying container4 128.8.238.181
+in for loop 5
+deploying container5 128.8.238.108
+deploying container1 128.8.238.183
+deploying container3 128.8.238.51
+deploying container1 128.8.238.183
+deploying container5 128.8.238.108
+in for loop 1
+deploying container1 128.8.238.183
+in for loop 2
+deploying container2 128.8.238.32
+in for loop 3
+deploying container3 128.8.238.51
+in for loop 4
+deploying container4 128.8.238.181
+in for loop 5
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+deploying container1 128.8.238.183
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+deploying container1 128.8.238.183
+deploying container5 128.8.238.108
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+deploying container1 128.8.238.183
+deploying container5 128.8.238.108
+deploying container2 128.8.238.32
+deploying container1 128.8.238.183
+deploying container4 128.8.238.181
+deploying container3 128.8.238.51
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+deploying container3 128.8.238.51
+deploying container5 128.8.238.108
+deploying container3 128.8.238.51
+deploying container2 128.8.238.32
+deploying container4 128.8.238.181
+deploying container1 128.8.238.183
+deploying container5 128.8.238.108
+deploying container1 128.8.238.183
+deploying container2 128.8.238.32
+deploying container4 128.8.238.181
+deploying container3 128.8.238.51
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+deploying container3 128.8.238.51
+deploying container3 128.8.238.51
+deploying container2 128.8.238.32
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+deploying container3 128.8.238.51
+in for loop 1 ---- 128.8.238.183
+deploying container1 128.8.238.183
+in for loop 2 ---- 128.8.238.32
+deploying container2 128.8.238.32
+in for loop 3 ---- 128.8.238.51
+deploying container3 128.8.238.51
+in for loop 4 ---- 128.8.238.181
+deploying container4 128.8.238.181
+in for loop 5 ---- 128.8.238.108
+deploying container5 128.8.238.108
+deploying container5 128.8.238.108
